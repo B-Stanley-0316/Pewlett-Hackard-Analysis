0a32df84a0cc4d378bf04631b93f676bc3bbd7b6
Brittney Stanley <brittneyrstanley07@hotmail.com>
2022-04-11 22:03:06 -0400

    Updating Queries Code.

--- Pewlett-Hackard-Analysis Folder/Queries/queries.sql
+++ Pewlett-Hackard-Analysis Folder/Queries/queries.sql
@@ -16,12 +16,12 @@ CREATE TABLE employees (
 );
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+		emp_no INT NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
+		PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
@@ -32,19 +32,21 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 CREATE TABLE Dept_Emp (
-	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no)
+emp_no INT NOT NULL,
+		dept_no VARCHAR NOT NULL,
+		from_date DATE NOT NULL,
+		to_date DATE NOT NULL,
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
-CREATE TABLE Titles (
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
+);
 SELECT * 
-FROM departments;
+FROM dept_emp;
